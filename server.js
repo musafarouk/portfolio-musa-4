19b862400ad41af772eee4824bebce2c836089cf
MusaFarouk <R3N3GAD3@DevlinBlake.local>
2017-09-18 01:46:17 +0100

    This version should last

---
+++ server.js
@@ -0,0 +1,28 @@
+//Begin server.js here
+const  express = require('express'),
+	   morgan = require('morgan')
+	   path = require('path');
+
+
+//making intsace of express app 
+const app = express()
+
+//setting up middleware 
+//logger 
+app.use(morgan('dev'));
+//TODO: static file middlewar
+app.use(express.static(path.join(__dirname,'public')));
+
+// app.use(express.static(path.join(__dirname, 'assets')));
+// app.set('views', './public')
+
+//test route 
+app.get('/', (req, res) => {
+	res.render('index')
+})
+//Select port to use
+const port = process.env.PORT || 3004
+
+//starting the server 
+app.listen(port, () => console.log(`server tsrting on localhost:${port}`))
+
